cc866010d58ce7c3a903a5b8334fdd10bce12ef9
Leonardo <cifuentes.k.l@gmail.com>
2020-10-06 20:16:37 -0300

    Se hizo formulario para agregar nueva categoría

--- client/src/componentes/productos.js
+++ client/src/componentes/productos.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
-
-
-
-export default function Productos(props){
-   
+export default function Productos(props){ 
 
         return(
             <div>
-                <h1>PRESENTANDO</h1>
                 <h2>{props.titulo}</h2>
                 <img>{props.imagen}</img>
                 <h3>{props.precio}</h3>
@@ -17,4 +12,4 @@ export default function Productos(props){
             </div>
                       
         )
-    }
+}
