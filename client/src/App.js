58d880bc5a8bae5466cbbed80c2f643831cf56b3
david <davidagustinhemmings@gmail.com>
2020-10-07 15:12:54 -0300

    se creo componente catalogo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-
 
 function App() {
   return (
     <div>
-
+     
     </div>
   );
 }
--- client/src/componentes/Producto.js
+++ client/src/componentes/Producto.js
@@ -1,5 +1,6 @@
 import React from 'react';
-  
+
+// componente presentacional de producto
 export default function Productos(props){ 
   return(
     <div>
