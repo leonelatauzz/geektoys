d57c2f17369db5a4135069b9efea1926b3f65af9
david <davidagustinhemmings@gmail.com>
2020-10-06 21:28:32 -0300

    commit

--- api/src/models/Categorias.js
+++ api/src/models/Categorias.js
@@ -1,20 +1,29 @@
-const { DataTypes } = require('sequelize');
+ const { DataTypes } = require('sequelize');
+ //const db = require('../db');
+ //const producto = db.model('product')
+ //let categorias;
 
-module.exports = (sequelize) => {
+ module.exports = (sequelize) => {
 
-    sequelize.define('categorias', {
-        id:{
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true  
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        descripcion: {
-            type: DataTypes.STRING  
-        }
-      });
-    };
+     sequelize.define('categorias', {
+         id:{
+             type: DataTypes.INTEGER,
+             autoIncrement: true,
+             primaryKey: true  
+         },
+         name: {
+           type: DataTypes.STRING,
+           allowNull: false,
+         },
+         descripcion: {
+             type: DataTypes.STRING  
+         }
+       });
+     };
+
+
+//   //categorias.belongsToMany(producto, { through: 'ProductoCategoria' });
+//   producto.belongsToMany(categorias, { through: 'ProductoCategoria' });
+
     
+
