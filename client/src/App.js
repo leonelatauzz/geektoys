7088e757dd3d9f678a35f649d777fcc6325c38e0
Leonardo <cifuentes.k.l@gmail.com>
2020-10-07 12:06:26 -0300

    agregamos un handler change al formulario

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Productos from './componentes/productos.js';
-import Categorias from './componentes/catalogos'
+
 
 function App() {
   return (
     <div>
-      <Productos />
-      <Categorias />
+      
     </div>
   );
 }
