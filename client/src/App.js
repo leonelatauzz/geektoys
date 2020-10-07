667929984678d8a6799f5b67d59b8ee9a568e28c
hikarigoth <alonsov.yamila@gmail.com>
2020-10-07 16:04:18 -0300

    Agregamos funcionalidad de agregar categorias a los formularios crud

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div>
-
+  
     </div>
   );
 }
