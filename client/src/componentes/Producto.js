f8b4721383f325fc1ad48a196399083ccd4ccc64
juanCruzRoldanAbiakel <juan-cruz-222@hotmail.com>
2020-10-08 02:00:12 -0300

    Filtrar por Categoria

--- client/src/componentes/Producto.js
+++ client/src/componentes/Producto.js
@@ -7,7 +7,7 @@ export default function Productos(props){
   let productosTest = [{name: "hola", title:"soy titulo", price:"200", stock:"20"}, {name: "soso", title:"titulotitulo", price:"120", stock:"15"}]
    return(
      <div>
-     { productosTest.length === 0 ? <div>No hay productos</div> : productosTest.map( productos => { return <ProductCard props={producto} />}) }
+     { productosTest.length === 0 ? <div>No hay productos</div> : productosTest.map( productos => { return <ProductCard props={productos} />}) }
    
 </div>
        
--- client/src/componentes/catalogo.jsx
+++ client/src/componentes/catalogo.jsx
@@ -3,6 +3,7 @@ import ProductCard from './productCard.jsx'
 
 // llega un array en props, se recorre y se renderiza en cada card
 export default function Catalogo(props){
+    
     return(
         <div className="card">
            {props.titulo.map((p)=> <ProductCard 
@@ -11,6 +12,12 @@ export default function Catalogo(props){
            imagen={p.imagen}
            />
         )}
+
+       {/*FILTRADO PARA EL CATALOGO POR CATEGORIA*/}
+        <div className = 'Category' {...ProductCard.filter(producto =>
+        producto.includes(props.titulo, props.descripcion).map(productoFilter =>(<ProductCard {...productoFilter}/>)))} 
+        ></div>
         </div>
+        
     )
 }
