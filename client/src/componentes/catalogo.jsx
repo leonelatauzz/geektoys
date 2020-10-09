dfd86bfca13df63f8f4ae25882d2fca31fcbbcb9
david <davidagustinhemmings@gmail.com>
2020-10-09 01:59:41 -0300

    hoy se duerme en paz

--- client/src/componentes/catalogo.jsx
+++ client/src/componentes/catalogo.jsx
@@ -3,20 +3,15 @@ import ProductCard from './productCard.jsx'
 
 // llega un array en props, se recorre y se renderiza en cada card
 export default function Catalogo(props){
-    
     return(
         <div className="card">
-           {props.productos.map((p)=> <ProductCard 
+           {props.productos.map((p)=> <ProductCard
+           key={p.id} 
            name ={p.name}
            description={p.description}
            picture={p.picture}
            />
         )}
-
-       {/*FILTRADO PARA EL CATALOGO POR CATEGORIA*/}
-        <div className = 'Category' {...ProductCard.filter(producto =>
-        producto.includes(props.name, props.description).map(productoFilter =>(<ProductCard {...productoFilter}/>)))} 
-        ></div>
         </div>
         
     )
