5a83c30ff394ce2c5df866536692bc1133a506fb
Leonardo <cifuentes.k.l@gmail.com>
2020-10-09 23:44:38 -0300

    Se termino de agregar detalles funcionales a catalogo y product card (links y stock y precio)

--- client/src/componentes/catalogo.jsx
+++ client/src/componentes/catalogo.jsx
@@ -3,15 +3,26 @@ import ProductCard from './productCard.jsx'
 
 // llega un array en props, se recorre y se renderiza en cada card
 export default function Catalogo(props){
+    function example(props) {
+        return props.stock == 0 ? 'Producto sin stock'
+             : props.stock == 1 ? 'Última unidad disponible'
+             : props.stock <= 5 ? props.stock + ' unidades disponibles'
+             : '';
+    }
+   
+                    
     return(
         
-        <div className="card">
+        <div className="card" >
 
            {props.productos.map((p)=> <ProductCard
-           key={p.id} 
-           name ={p.name}
+           key={p.id}
+           id={p.id} 
+           name ={p.name }
            description={p.description}
            picture={p.picture}
+           price={p.price}
+           stock={example(p)}
            />
         )}
         </div>
--- client/src/componentes/productCard.jsx
+++ client/src/componentes/productCard.jsx
@@ -4,9 +4,11 @@ import Productos from './Producto.js';
 
 // se crea diseño de productos en una card utilizando bootstrap
 export default function ProductCard (props){
+    
+    
     return (
         <div className="container">
-          <div className="row">
+          <div className="row">                 
               <div className="col-sm">
                   <div className="card" >
                       <div>
@@ -17,10 +19,12 @@ export default function ProductCard (props){
                                   </div>
                                   <div className="col-md-8">
                                       <div className="card-body">
-                                          <h5 className="card-title">{props.name}</h5>
+                                          <h5 className="card-title"><a href= {`http://localhost:3000/products/prod/${props.id}`} >{props.name}</a></h5>
                                             <p className="card-text">{props.description}</p>
-                                          <p className="card-text"><small className="text-muted">precio: 6000</small></p>
-                                          <Productos/>
+                                            <p className="card-text"><small className="text-muted">${props.price}</small></p>
+                                            <p className="card-text"><small className="text-muted">{props.stock}</small></p>
+                                        {/*EL componente <Product /> se renderiza solo cuando vamos a la ruta del producto con su id,
+                                         a traves de routing, aca no se necesita*/}  
                                       </div>
                                   </div>
                               </div>
