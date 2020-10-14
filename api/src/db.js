33630f7d550273b3f72fecdd80c2205dd3b82f2d
david <davidagustinhemmings@gmail.com>
2020-10-13 21:08:26 -0300

    se creo modelo usuario

--- api/src/db.js
+++ api/src/db.js
@@ -32,6 +32,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // Para relacionarlos hacemos un destructuring
 const { Product } = sequelize.models;
 const { Category } = sequelize.models;
+const { Users } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
---
+++ api/src/models/Users.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Users = sequelize.define('users', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return Users;
+}
