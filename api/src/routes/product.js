3e738d510f7844b5abf4c7ef811828eb0a0fc709
Fidel Villegas <fidel.villegaz@gmail.com>
2020-10-22 22:25:59 -0500

    Se crea ruta para modificar review

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -135,6 +135,29 @@ server.put("/category/:id", (req, res) => {
   })
 })
 
+server.put('/:id/review/:idReview', (req,res) => {
+  const {rating,description} = req.body;
+    Product.findByPk(req.params.id)
+    .then(product =>{
+      if(!product){
+        res.status(404).send('Producto no encontrado');
+      }else{
+        Review.findByPk(req.params.idReview)
+        .then(review => {
+          if(!review){
+            res.status(404).send('Review no encontrada');
+          }else{        
+          review.rating = rating;
+          review.description = description;
+          review.save();
+          res.status(201).send('La review fue modificada correctamente');
+          }
+        })
+      }
+    })
+})
+
+
 server.put("/:id", upload.single('images'), (req, res) => {
   let pic;
   let product;
